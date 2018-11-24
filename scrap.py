dd69482941054434efb1b9546403a75427a5be1e
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-11-25 00:51:16 +0700

    Merdeka v2 otw. So sleepy

--- scrap.py
+++ scrap.py
@@ -17,40 +17,75 @@ def getNews(url):
     data = requests.get(url)
     data = data.content
     soup = BeautifulSoup(data, "html.parser")
-    title = getContent(soup.find("meta", {"property": "og:title"}))
-    url = getContent(soup.find("meta", {"property": "og:url"}))
-    image = getContent(soup.find("meta", {"property": "og:image"}))
-    author = getContent(soup.find("meta", {"name": "author"}))
-    news = parseNews(soup.find("div", {"class": "mdk-body-paragpraph"}))
 
-    result = {
-        "title": title,
-        "url": url,
-        "image": image,
-        "author": author,
-        "news": news
-    }
+    if soup.find("ul", {"class": "mdk-list-paging"}) is None:
+        title = getContent(soup.find("meta", {"property": "og:title"}))
+        url = getContent(soup.find("meta", {"property": "og:url"}))
+        image = getContent(soup.find("meta", {"property": "og:image"}))
+        author = getContent(soup.find("meta", {"name": "author"}))
+        news = parseNews(soup.find("div", {"class": "mdk-body-paragpraph"}))
 
-    # print("LAH")
+        result = {
+            "title": title,
+            "url": url,
+            "image": image,
+            "author": author,
+            "news": news
+        }
 
-def parseNews(html):
-    s = []
-    html.find("div", {"class": "title-section_terkait"}).decompose()
-    html.find("ul", {"id": "list-section_terkait"}).decompose()
-    html.find("div").unwrap()
-    for i in html.get_text().split("\n"):
-        if i.strip():
-            i = re.sub(" \[(.*?)]", "", i)
-            s.append(i)
-    text_file = open("output_test.txt", "w")
-    text_file.write('\n'.join(map(str, s)))
-    text_file.close()
-    print(s)
-    # return html
+        print(result)
+    else:
+        a = soup.select("ul[class=mdk-list-paging] li a")
+        regex_arrow = re.compile('<a class="arrow.*"></a>')
+        regex_intro = re.compile('<a href=".*">Intro</a>')
+        filtered = [str(i) for i in a if not regex_arrow.search(str(i)) and not regex_intro.search(str(i))]
+
+        for i in filtered:
+            search = re.compile('(?<=href=").*?(?=")')
+            data = requests.get(search.search(i).group(0))
+            data = data.content
+            soup = BeautifulSoup(data, "html.parser")
+            title = getContent(soup.find("meta", {"property": "og:title"}))
+            url = getContent(soup.find("meta", {"property": "og:url"}))
+            image = getContent(soup.find("meta", {"property": "og:image"}))
+            author = getContent(soup.find("meta", {"name": "author"}))
+            news = parseNews(soup.find("div", {"class": "mdk-body-paragpraph"}), "paging")
+
+            result = {
+                "title": title,
+                "url": url,
+                "image": image,
+                "author": author,
+                "news": news
+            }
+
+            print(news)
+
+def parseNews(html, article_type = "normal"):
+    if article_type is "normal":
+        s = []
+        html.find("div", {"class": "title-section_terkait"}).decompose()
+        html.find("ul", {"id": "list-section_terkait"}).decompose()
+        html.find("div").unwrap()
+        for i in html.get_text().split("\n"):
+            if i.strip():
+                i = re.sub(" \[(.*?)]", "", i)
+                s.append(i)
+        s = '\n'.join(map(str, s))
+        text_file = open("output_test.txt", "w")
+        text_file.write(s)
+        text_file.close()
+    else:
+        s  = {}
+        title = html.find("h6").get_text()
+        title = re.sub("\d\. ", "", title)
+        print(title)
+
+    # return s
 
 def getContent(html):
     return html['content']
 
 if __name__ == "__main__":
     # search("https://www.merdeka.com/cari/?q=prabowo")
-    getNews("https://www.merdeka.com/politik/prabowo-kesulitan-dana-kampanye-minta-kredit-dari-bank-indonesia-enggak-dapat.html")
+    getNews("https://www.merdeka.com/politik/prediksi-prediksi-prabowo-subianto-yang-mengejutkan.html")
